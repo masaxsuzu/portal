d7606f63bb3e08c4ce14cf091bdc762c7136ed5b
masaxsuzu <masaxsuzu+github@gmail.com>
2025-05-05 22:50:43 +0900

    Refine index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,39 @@
+import Head from "next/head";
+import Image from "next/image";
+import data from "./data.json";
+import About from "../components/About";
+import MainCta from "../components/MainCta";
+import Divider from "../components/Divider";
+import Cards from "../components/Cards";
+type ComponentMapKeyType =
+  | "About"
+  | "MainCta"
+  | "Divider"
+  | "Cards";
+
+const componentMap = {
+  About,
+  MainCta,
+  Divider,
+  Cards,
+};
+
 export default function HomePage() {
   return (
-    <div className="underline" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f8f9fa'}}>
-      <h1>masaxsuzu</h1>
+    <div className="bg-background flex justify-center w-full">
+      <Head>
+        <title>{data?.title}</title>
+        <meta name="description" content={data?.siteDescription} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="my-10  text-primary max-w-[1024px] flex flex-col items-start px-6">
+        {data?.components?.map((item, idx) => {
+          const key = item?.name as ComponentMapKeyType;
+          const Comp = componentMap[key];
+
+          return <Comp key={idx} data={item?.data as any} />;
+        })}
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,48 @@
+import type { Config } from 'tailwindcss'
+
+const colors = {
+  background: "#13111A",
+  primary: "#B7B4C7",
+  white: "#FFFFFF",
+  cardbg: "#1D1A27",
+  skyblue: "#94C1F2",
+  cardborder: "#ffffff0f",
+  article: "#dd62a7",
+  npm: "#db626f",
+  lib: "#ebda69",
+  csharp: "#178600",
+  c: "#555555",
+  rust: "#dea584",
+}
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors,
+      fontSize: {
+        sm: "12px",
+      },
+      fontFamily: {
+        default: ["Inter", "sans-serif"],
+      },
+      fontWeight: {
+        thin: "100",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+      },
+      screens: {
+        sm: { max: "640px" },
+        md: { min: "640px" },
+        lg: { min: "1024px" },
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
